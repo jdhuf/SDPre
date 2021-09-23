581f4ff3c4f8d9b26cc2bf9c41a2a21ac4b0139d
jdhuf <jdhufstetler@gmail.com>
2021-09-23 02:47:03 -0400

    complete objective1lab1

--- Labs/Objective1Lab1.java
+++ Labs/Objective1Lab1.java
@@ -1,5 +1,5 @@
 public class Objective1Lab1 {
     public static void main(String[] args) {
-      System.out.println("I'm not sure what the starter code from the lab material is or where I go to complete the lab.");
+      System.out.println("Hello, Skill Distilley");
     }
 }
