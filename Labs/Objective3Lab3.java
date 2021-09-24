cef55c3de6c5b68a1d29c98daccfbc3c3ec6a0fe
jdhuf <jdhufstetler@gmail.com>
2021-09-23 23:31:11 -0400

    complete objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,13 @@
+public class Objective3Lab3 {
+  public static void main(String[] args){
+    int first = 1 + (int)(Math.random() * 6);
+    int second = 1 + (int)(Math.random() * 6);
+    int third = 1 + (int)(Math.random() * 6);
+
+    int sum = first + second + third;
+
+    String printedEquation = first + " + " + second + " + " + third + " = " + sum;
+
+    System.out.println(printedEquation);
+  }
+}
