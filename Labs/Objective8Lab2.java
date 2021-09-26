9eca9330634e68df559bd1c173cab5d5c694285e
jdhuf <jdhufstetler@gmail.com>
2021-09-25 23:58:37 -0400

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,12 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int total = 0;
+
+    for (int i = 1; i < 21; i++){
+
+      total += i;
+    }
+    
+    System.out.println(total);
+  }
+}
